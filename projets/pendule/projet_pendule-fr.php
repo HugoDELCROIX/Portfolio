6b5c14ed7dd96e048e1ebd71173691de9debc792
HugoDELCROIX <hugo.delcroix@outlook.com>
2022-02-06 16:40:19 +0100

    ajout video pendule

--- projets/pendule/projet_pendule-fr.php
+++ projets/pendule/projet_pendule-fr.php
@@ -16,10 +16,26 @@
     <div id="bloc">
         <h1>Projet de création d'un pendule oscillatoire simple</h1>
         <img src="../../images/pendule.png" alt="Schéma du pendule">
-        </iframe>
+        <button onclick="playPause()">Play/Pause</button>
+        <video id="video1" width="420">
+            <source src="../pendule/Projet Science 3.mp4" type="video/mp4">
+            Your browser does not support HTML video.
+        </video>
+
+        <script> 
+var myVideo = document.getElementById("video1"); 
+
+function playPause() { 
+  if (myVideo.paused) 
+    myVideo.play(); 
+  else 
+    myVideo.pause(); 
+}  
+</script> 
+
     </div>
     <div id="bloc">
-        <iframe src="/projets/pendule/Projet_Science_3.pdf" width="100%" height="1000px">
+        <iframe src="/projets/pendule/Projet_Science_3.pdf" width="100%" height="1000px"></iframe>
     </div>
 </body>
 
