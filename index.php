145a9d0bba4d848dcdc06b3a960e1f4f4cc8c1d9
HugoDELCROIX <hugo.delcroix@outlook.com>
2022-03-04 17:30:28 +0100

    projects

--- index.php
+++ index.php
@@ -29,9 +29,12 @@
         <h1>Hugo Delcroix</h1>
         <h2>Étudiant en Informatique et Réseaux</h2>
         <div class="logo">
-            <a target="_blank" id='deco_linkedin' href="https://www.linkedin.com/in/hugo-delcroix/"><i class="fa-brands fa-linkedin"></i></a>
-            <a target="_blank" id='deco_github' href="https://github.com/HugoDELCROIX"><i class="fa-brands fa-github"></i></a>
-            <a target="_blank" id='deco_insta' href="https://www.instagram.com/hugo.delcroix_pro/"><i class="fa-brands fa-instagram"></i></a>
+            <a target="_blank" id='deco-linkedin' href="https://www.linkedin.com/in/hugo-delcroix/"><i
+                    class="fa-brands fa-linkedin"></i></a>
+            <a target="_blank" id='deco-github' href="https://github.com/HugoDELCROIX"><i
+                    class="fa-brands fa-github"></i></a>
+            <a target="_blank" id='deco-insta' href="https://www.instagram.com/hugo.delcroix_pro/"><i
+                    class="fa-brands fa-instagram"></i></a>
         </div>
     </header>
 
@@ -74,7 +77,7 @@
                 dans le développement Web que ce soit <b>frontend ou backend</b> .</p>
         </div>
         <h1><i class="fa-solid fa-bookmark"></i> Mes Centres d’intérêt : </h1>
-        <div class="logo_list">
+        <div class="logo-list">
             <img src="images/logo/apple.png" alt="logo_apple">
             <img src="images/logo/tesla.png" alt="logo_tesla">
             <img src="images/logo/linux.png" alt="logo_linux">
@@ -84,7 +87,7 @@
             <img src="images/logo/worldcup_logo.png" alt="logo_WorldCup">
         </div>
         <h1><i class="fa-solid fa-screwdriver-wrench"></i> Mes Outils : </h1>
-        <div class="logo_list">
+        <div class="logo-list">
 
             <img src="images/logo/Fedora-logo.png" alt="logo_fedora">
             <img src="images/logo/vscode_logo.png" alt="logo_vscode">
@@ -98,8 +101,8 @@
     <section>
         <h1><i class="fa-solid fa-file-lines"></i> Mes compétences </h1>
 
-        <div id="container_skills">
-            <div id="list_right">
+        <div id="container-skills">
+            <div id="list-right">
                 <h3>Ce que je sais utiliser</h3>
                 <li>HTML/CSS</li>
                 <li>JAVA</li>
@@ -109,7 +112,7 @@
                 <li>Pack Office</li>
             </div>
             <hr>
-            <div id="list_left">
+            <div id="list-left">
                 <h3>Ce que j'apprends à utiliser</h3>
                 <li>PHP</li>
                 <li>MongoDB</li>
@@ -122,74 +125,73 @@
 
     <section>
         <h1><i class="fas fa-user-graduate"></i> Éducation</h1>
-        
-            <div class="container-img-text container-formation">
-                <img src="images/logo/logo_VIA.png" alt="logo_esaip">
-                <div class="list_formation">
-                    <h4><b>VIA University College</b> <br> </h4>
-                    <p>
-                        <b>Février à Juin 2022 :</b> Ingénierie Logicielle :<br>
-                    </p>
-                    <ul>
-                        <li>Développement Android</li>
-                        <li>Informatique Décisionnelle</li>
-                        <li>Bases de données non SQL et relationnelles</li>
-                        <li>Conception Web réactive</li>
-                        <li>Processus d'ingénierie Web</li>
-                        <li>Management de projets en Ingénierie Informatique</li>
-                        <li>Création d'un blog sur le séjour d'études <a href="#mes_projets"> (En savoir plus)</a></li>
-                    </ul>
-                </div>
 
-            </div>
-            <div class="container-img-text container-formation">
-                <img src="images/logo/logo_ESAIP_INGENIEUR_RVB_2016.png" alt="logo_esaip">
-                <div class="list_formation">
-                    <h4><b>ESAIP, école d'ingénieurs du numérique et de la prévention des risques : </b> <br> </h4>
-                    <p>
-                        <b>2021 - Aujourd'hui :</b> Cycle Ingénieur du numérique : <br>
-                    </p>
-                    <ul>
-                        <li>Analyse mathématiques et Algèbre</li>
-                        <li>Dévelopement Web frontend : HTML/CSS</li>
-                        <li>Programmation orientée objet : JAVA</li>
-                        <li>Gestion de Bases de données : SQL</li>
-                    </ul>
-                    <p>
-                        <b>2019 - 2021 :</b> Classe préparatoire :
-                    </p>
-                    <ul>
-                        <li>Analyse mathématiques et Algèbre</li>
-                        <li>Mécanique des mouvements</li>
-                        <li>Apprentissage de l'Anglais</li>
-                        <li>Introduction à la gestion de projets</li>
-                        <li>Science de l'entreprise et éthique professionnelle</li>
-                        <li>Stage en entreprise de 1 mois <a href="#exp_pro"> (En savoir plus)</a></li>
-                    </ul>
-                </div>
-
-            </div>
-            <div class="container-img-text container-formation">
-                <img src="images/logo/gouv_logo.png"
-                    alt="logo_gouv">
+        <div class="container-img-text container-list">
+            <img src="images/logo/logo_VIA.png" alt="logo_esaip">
+            <div class="list-formation">
+                <h4><b>VIA University College</b> <br> </h4>
                 <p>
-                    <b> 2019 :</b> Obtention d'un bac scientifique mention Bien au sein du <b>Lycée Lasalle
-                        Saint-Charles</b>. <br>
-                    <b> 2017 & 2018 :</b> Voyage scolaire en Afrique du Sud avec obtention d'une <b>certification de
-                        niveau d'anglais B1 </b>. <br>
-                    <b> 2015 :</b> Voyage scolaire en Australie. <br>
+                    <b>Février à Juin 2022 :</b> Ingénierie Logicielle :<br>
                 </p>
-
+                <ul>
+                    <li>Développement Android</li>
+                    <li>Informatique Décisionnelle</li>
+                    <li>Bases de données non SQL et relationnelles</li>
+                    <li>Conception Web réactive</li>
+                    <li>Processus d'ingénierie Web</li>
+                    <li>Management de projets en Ingénierie Informatique</li>
+                    <li>Création d'un blog sur le séjour d'études <a href="#mes_projets"> (En savoir plus)</a></li>
+                </ul>
             </div>
-        
+
+        </div>
+        <div class="container-img-text container-list">
+            <img src="images/logo/logo_ESAIP_INGENIEUR_RVB_2016.png" alt="logo_esaip">
+            <div class="list-formation">
+                <h4><b>ESAIP, école d'ingénieurs du numérique et de la prévention des risques </b> <br> </h4>
+                <p>
+                    <b>2021 - Aujourd'hui :</b> Cycle Ingénieur du numérique : <br>
+                </p>
+                <ul>
+                    <li>Analyse mathématiques et Algèbre</li>
+                    <li>Dévelopement Web frontend : HTML/CSS</li>
+                    <li>Programmation orientée objet : JAVA</li>
+                    <li>Gestion de Bases de données : SQL</li>
+                </ul>
+                <p>
+                    <b>2019 - 2021 :</b> Classe préparatoire :
+                </p>
+                <ul>
+                    <li>Analyse mathématiques et Algèbre</li>
+                    <li>Mécanique des mouvements</li>
+                    <li>Apprentissage de l'Anglais</li>
+                    <li>Introduction à la gestion de projets</li>
+                    <li>Science de l'entreprise et éthique professionnelle</li>
+                    <li>Stage en entreprise de 1 mois <a href="#exp_pro"> (En savoir plus)</a></li>
+                </ul>
+            </div>
+
+        </div>
+        <div class="container-img-text container-list">
+            <img src="images/logo/gouv_logo.png" alt="logo_gouv">
+            <p>
+                <b> 2019 :</b> Obtention d'un bac scientifique mention Bien au sein du <b>Lycée Lasalle
+                    Saint-Charles</b>. <br>
+                <b> 2017 & 2018 :</b> Voyage scolaire en Afrique du Sud avec obtention d'une <b>certification de
+                    niveau d'anglais B1 </b>. <br>
+                <b> 2015 :</b> Voyage scolaire en Australie. <br>
+            </p>
+
+        </div>
+
     </section>
 
     <section>
         <div id="exp_pro">
             <h1><i class="fas fa-hard-hat"></i> Expérience professionnelle</h1>
-            <div id="contenu_experience">
+            <div class="container-img-text container-list">
                 <img src="images/kdr.png" alt="logo_KDR">
-                <div class="liste">
+                <div class="list">
                     <h4><b>Stage : </b> </h4>
                     <p>
                         <b>Avril 2020</b> - <b>K.D.R Électricité : </b> Magasin de matériel électrique et de chantier
@@ -205,44 +207,53 @@
     </section>
 
     <section>
-        <div id="mes_projets"></div>
         <h1><i class="fas fa-project-diagram"></i> Mes projets</h1>
-        <div class="box_projet">
-            <div id="liste_projet">
-                <a href="projets/pendule/projet_pendule-fr.php" target="blank">
-                    <div class="projet projet-1">
-                        <div class="misc">
-                            <div class="misctext">
-                                <h4 class="title">
-                                    Création d'un pendule oscillatoire
-                                </h4>
-                            </div>
-                        </div>
+        <div id="container-projects">
+            <a href="projets/pendule/projet_pendule-fr.php" target="blank">
+                <div class="container-img-text container-card">
+                    <img src="images/miniature_pendule.png" alt="">
+                    <div class="description">
+                        <h4>
+                            Création d'un pendule oscillatoire
+                        </h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, recusandae. Deleniti ipsa
+                            dolores, sed ipsum velit iure accusamus ex quas sit, obcaecati quae, omnis illum
+                            consequuntur earum illo commodi repellendus?
+                        </p>
                     </div>
-                </a>
-                <a href="projets/huitre/index.html" target="blank">
-                    <div class="projet projet-2">
-                        <div class="misc">
-                            <div class="misctext">
-                                <h4 class="title">
-                                    Création d'un site en utilisant la technologie HTML/CSS
-                                </h4>
-                            </div>
-                        </div>
+                </div>
+            </a>
+            <a href="projets/huitre/index.html" target="blank">
+                <div class="container-img-text container-card">
+                    <img src="images/huitre1.jpg" alt="">
+                    <div class="description">
+                    <h4>
+                        Création d'un site en utilisant la technologie HTML/CSS
+                    </h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, recusandae. Deleniti ipsa
+                        dolores, sed ipsum velit iure accusamus ex quas sit, obcaecati quae, omnis illum
+                        consequuntur earum illo commodi repellendus?
+                    </p>
                     </div>
-                </a>
-                <a href="http://horsensei.online" target="blank">
-                    <div class="projet projet-3">
-                        <div class="misc">
-                            <div class="misctext">
-                                <h4 class="title">
-                                    Blog sur mon séjour d'études au Danemark
-                                </h4>
-                            </div>
-                        </div>
+                </div>
+            </a>
+            <a href="http://horsensei.online" target="blank">
+                <div class="container-img-text container-card">
+                    <img src="images/drapeau_danemark.jpeg" alt="">
+                    <div class="description">
+                    <h4>
+                        Blog sur mon séjour d'études au Danemark
+                    </h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, recusandae. Deleniti ipsa
+                        dolores, sed ipsum velit iure accusamus ex quas sit, obcaecati quae, omnis illum
+                        consequuntur earum illo commodi repellendus?
+                    </p>
                     </div>
-                </a>
-            </div>
+                </div>
+            </a>
         </div>
     </section>
 
