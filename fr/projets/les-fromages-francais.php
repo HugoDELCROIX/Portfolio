66b1824e90cf0eae615f87297c1ba9e133ef5465
HugoDELCROIX <hugo.delcroix@hotmail.com>
2022-03-31 01:39:02 +0200

    finished

---
+++ fr/projets/les-fromages-francais.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/mediaqueries/iphone12.css">
+    <link rel="stylesheet" href="../../css/style_contact.css">
+    <title>Les Fromages Français</title>
+</head>
+
+<body>
+    <section>
+        <div class="container-projects">
+            <div class="container-img-text container-card-no-hover comparaison-projects">
+                <a href="../projets/Les-Fromages-Francais/" target="_blank">
+                    <div class="img-description">
+                        <img src="../projets/Les-Fromages-Francais/images/munster.jpg" alt="munster_fromage">
+                        <h6>Version native HTML/CSS</h6>
+                    </div>
+                </a>
+                <a href="../projets/Les-Fromages-Francais-Bootstrap-version/" target="_blank">
+                    <div class="img-description">
+                        <img src="../../images/munster-bootstrap.jpg" alt="pendule">
+                        <h6>Version Bootstrap</h6>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </section>
+    <button><a href="../../index.php">Revenir à la page principale</a></button>
+</body>
+
+</html>
