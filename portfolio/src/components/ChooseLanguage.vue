353823cd937295bdcaf2a3770ff564caea31318d
HugoDELCROIX <hugo.delcroix@proton.me>
2023-03-09 19:56:48 +0100

    all files deleted

--- portfolio/src/components/ChooseLanguage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex items-center justify-between w-52 m-auto mt-4 mb-4">
-    <a href="">English</a>
-    <font-awesome-icon icon="fa-solid fa-earth-americas" />
-    <a href="">Français</a>
-  </div>
-</template>
-
-<script setup></script>
