6aa50ed2fabbf195eea3e02bd2cd3c114b938b63
HugoDELCROIX <hugo.delcroix@proton.me>
2023-07-27 11:06:01 +0200

    patched responsive

--- src/components/FooterVue.vue
+++ src/components/FooterVue.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="flex justify-around text-dark_blue dark:text-white text-sm mb-3">
+  <div
+    class="flex flex-col sm:flex-row sm:justify-around items-center justify-center text-dark_blue dark:text-white text-sm mb-3"
+  >
     <div class="flex items-center gap-2">
       <p>{{ data.footer.vue }}</p>
       <font-awesome-icon class="text-2xl" icon="fa-brands fa-vuejs" />
+    </div>
+    <div class="flex items-center gap-2">
+      <a href="https://creativecommons.org/licenses/by-nc/4.0/">{{ data.footer.rights }}</a>
+      <font-awesome-icon class="text-xl" icon="fa-brands fa-creative-commons" />
+      <font-awesome-icon class="text-xl" icon="fa-brands fa-creative-commons-nc-eu" />
+      <font-awesome-icon class="text-xl" icon="fa-brands fa-creative-commons-by" />
     </div>
     <p>{{ version }}</p>
   </div>
